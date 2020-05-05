66b6fb20176e043159946a7c75548aeb631eaeeb
jrmcgarvey <jmcgarve@bellsouth.net>
2020-05-05 04:17:49 -0400

    some styling

--- app/controllers/icecreams_controller.rb
+++ app/controllers/icecreams_controller.rb
@@ -28,7 +28,7 @@ class IcecreamsController < ApplicationController
 
     respond_to do |format|
       if @icecream.save
-        format.html { redirect_to @icecream, notice: 'Icecream was successfully created.' }
+        format.html { redirect_to @icecream, notice: 'The ice cream order was successfully created.' }
         format.json { render :show, status: :created, location: @icecream }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class IcecreamsController < ApplicationController
   def update
     respond_to do |format|
       if @icecream.update(icecream_params)
-        format.html { redirect_to @icecream, notice: 'Icecream was successfully updated.' }
+        format.html { redirect_to @icecream, notice: 'The ice cream order was successfully updated.' }
         format.json { render :show, status: :ok, location: @icecream }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class IcecreamsController < ApplicationController
   def destroy
     @icecream.destroy
     respond_to do |format|
-      format.html { redirect_to icecreams_url, notice: 'Icecream was successfully destroyed.' }
+      format.html { redirect_to icecreams_url, notice: 'The ice cream order was successfully deleted.' }
       format.json { head :no_content }
     end
   end
--- app/models/icecream.rb
+++ app/models/icecream.rb
@@ -1,6 +1,10 @@
 class Icecream < ApplicationRecord
   FLAVORS = ["vanilla","chocolate","strawberry","pistachio"]
   SIZES = ["pint","quart","gallon"]
+  validates :flavor, presence: true
+  validates :size, presence: true
+  validates :count, presence: true
+  validates :name, presence: true
 
   def self.flavors
     two_d = []
