f8653f4173040ad621675b65829071de6ed49297
jrmcgarvey <jmcgarve@bellsouth.net>
2020-11-09 11:56:49 -0500

    add collection select

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_152510) do
+ActiveRecord::Schema.define(version: 2020_11_08_223634) do
+
+  create_table "flavors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "icecreams", force: :cascade do |t|
     t.string "name"
